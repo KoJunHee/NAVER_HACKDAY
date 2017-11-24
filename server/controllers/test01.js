bc17d3adc1bee8634eea218955c07abe84719863
junhee.ko <kjh338076@naver.com>
2017-11-25 05:06:42 +0900

    Update test01.js

--- server/controllers/test01.js
+++ server/controllers/test01.js
@@ -16,23 +16,8 @@ exports.getData = {
     auth: false,
     handler: (request, reply) => {
 
-        //1 wating
-        // var requestUrl = "http://ep.tricycle.co.kr/Product/Product_Halfclub_naverdb_All.TXT";
-
-        //2 wating
-        // var requestUrl = "http://ipp.interpark.com/partner/naver2/map_goods.txt";
-
-        //3 wating
-        // var requestUrl = "http://www.paintinfo.co.kr/ad/naver/naverfulldbV3.txt";
-
-        //4 wating
-        // var requestUrl = "http://www.lottemart.com/batchif/naver/naver_shopping_all.txt";
-
-        //5 ok
-        var requestUrl = "https://www.bobbibrown.co.kr/media/NAVER_EP/BBKR_EPURL_20170830.txt";
-
-        //6 ok
-        // var requestUrl = "https://www.rovectin.co.kr:443/shop/price/naver.php";
+        //url
+        // var requestUrl = "SECRET";
 
         //stream
         var stream = Request(requestUrl);
