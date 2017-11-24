48ee4d5eb95d384559305250e500c2df8976f97d
junhee.ko <kjh338076@naver.com>
2017-11-25 05:07:49 +0900

    Update test04.js

--- server/controllers/test04.js
+++ server/controllers/test04.js
@@ -17,27 +17,8 @@ exports.getData = {
     auth: false,
     handler: (request, reply) => {
 
-        //1 wating
-        // var requestUrl = "http://ep.tricycle.co.kr/Product/Product_Halfclub_naverdb_All.TXT";
-
-        //2 wating
-        // var requestUrl = "http://ipp.interpark.com/partner/naver2/map_goods.txt";
-
-        //3 wating
-        // var requestUrl = "http://www.paintinfo.co.kr/ad/naver/naverfulldbV3.txt";
-
-        //4 wating
-        //var requestUrl = "http://www.lottemart.com/batchif/naver/naver_shopping_all.txt";
-
-        //5 ok
-        var requestUrl = "https://www.bobbibrown.co.kr/media/NAVER_EP/BBKR_EPURL_20170830.txt";
-
-        //6 ok
-        // var requestUrl = "https://www.rovectin.co.kr:443/shop/price/naver.php";
-
-        //7 new
-        // var requestUrl = "http://api.shopping.naver.com/api/exportFeederHdfs/?authkey=61bdaabb277f13dbb7b5da4c502aa1fa&type=FILE&path=/user/irteam/20171122/0400/107396/input/107396.origin";
-
+        //url
+        // var requestUrl = "SECRET";
 
         //stream
         var stream = got(requestUrl);
